d869ec4fc472aa7b61c45d9fa7480268f5005934
Burak Kücük <kuecuek@digitalbar.de>
2026-06-08 15:25:00 +0200

    Remove basePath for custom domain

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/orientgrill1",
-  assetPrefix: "/orientgrill1/",
   images: {
     unoptimized: true,
   },
